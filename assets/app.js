e29aa59f30af99315efff5dcb2695a0c2b743bbc
Harith Onigemo <45686269+Harithmetic1@users.noreply.github.com>
2020-09-20 14:54:15 +0100

    Alium Landing Page

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+function clickOn() {
+    let menu = document.querySelector('.dropdown');
+    menu.classList.toggle("block");
+};
+function reveal(){
+    let next = document.querySelector('.footerlinks');
+    next.classList.toggle("blockF");
+}
+function reveal1(){
+    let next = document.querySelector('.footerlinks1');
+    next.classList.toggle("blockF1");
+}
+function reveal2(){
+    let next = document.querySelector('.footerlinks2');
+    next.classList.toggle("blockF2");
+}
